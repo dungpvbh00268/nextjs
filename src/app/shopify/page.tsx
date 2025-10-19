a83cd32935464d56e4119f7b3ca1acd304108f46
phamdung2209 <phamdung.22092003@gmail.com>
2025-10-19 21:17:59 +0700

    change position route

--- src/app/shopify/page.tsx
+++ src/app/shopify/page.tsx
@@ -12,14 +12,6 @@ const Page = () => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <pre>
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              {JSON.stringify(users, null, 2)}
-            </code>
-          </pre>
-        </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -41,6 +33,14 @@ const Page = () => {
             Users
           </Link>
         </div>
+
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <pre>
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              {JSON.stringify(users, null, 2)}
+            </code>
+          </pre>
+        </ol>
       </main>
     </div>
   );
