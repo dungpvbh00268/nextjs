53b827488a791360bf31bfa70c551a8308c78400
phamdung2209 <phamdung.22092003@gmail.com>
2025-10-19 21:17:29 +0700

    add link route

--- src/app/shopify/page.tsx
+++ src/app/shopify/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React, { use } from "react";
 
 const Page = () => {
@@ -17,6 +19,28 @@ const Page = () => {
             </code>
           </pre>
         </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <Link
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="/"
+          >
+            <Image
+              className="dark:invert"
+              src="./vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Go Home
+          </Link>
+          <Link
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="/users"
+          >
+            Users
+          </Link>
+        </div>
       </main>
     </div>
   );
