c1fe5e6c1c21d83b12d6f09db3ab7036ce7717ea
phamdung2209 <phamdung.22092003@gmail.com>
2025-10-26 15:36:51 +0700

    remove test

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { use } from "react";
+
 import prisma from "~/lib/prisma";
-import User from "./user";
 
 const Page = () => {
   const users = use(prisma.user.findMany());
@@ -10,7 +10,6 @@ const Page = () => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <User />
         <Image
           className="dark:invert"
           src="./next.svg"
